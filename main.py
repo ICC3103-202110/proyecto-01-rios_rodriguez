e40852f355632e42151e72b40baea3885321d213
feliperiosvaldes <frios1@miuandes.cl>
2021-04-13 22:55:20 -0300

    nuevas clases
    nuevas clases y random cards

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from person import Person
+from numpy import random
+from random import shuffle
 
 print("\n We must first start by creating the players.")
+print("Remember that only can play 3-4 players")
 people = []
 money = []
-cards = ["Duque", "Asesino", "Capitan", "Embajador", "Condesa", 
-        "Duque", "Asesino", "Capitan", "Embajador", "Condesa", 
-        "Duque", "Asesino", "Capitan", "Embajador", "Condesa"]
+cards_number = []
+cards = ["Duque", "Assassin", "Capitan", "Embajador", "Condesa", 
+        "Duque", "Assassin", "Capitan", "Embajador", "Condesa", 
+        "Duque", "Assassin", "Capitan", "Embajador", "Condesa"]
+
+def random_cards():
+    shuffle(cards)
+    print(cards)
 
 def print_menu_and_select():
     print("Select an option: ")
@@ -21,34 +29,33 @@ def create_person():
     money = 2
     people.append(Person(name, money))
 
-
 def show_people():
     print("\nPeople created:")
     for (i, _) in enumerate(people):
         print(f"{i}: {people[i].name} ")
 
-   
+
 def show_peoples_money():
     print("\n player coins at the moment:")
     for (i, _) in enumerate(people):
         print(f"{i}: {people[i].name} has {people[i].money} coins ")
 
-
 def menu():
     while True:
         selection = print_menu_and_select()
         if selection == 0:
-
-            break
+            if 3 <= len(people) <= 4:
+                break
+            else:
+                raise ValueError("Remember that you can only play with 3 or 4 players")
         if selection == 1:
             create_person()
         if selection == 2:
             show_people()
         if selection == 3:
             show_peoples_money()
-
-
+        if selection == 4:
+            random_cards()
 
 if __name__== "__main__":
     menu()
-    
