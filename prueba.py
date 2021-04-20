8948f6d532348409edf91db30628710b7378a3d4
feliperiosvaldes <frios1@miuandes.cl>
2021-04-19 22:13:39 -0300

    Agregue prueba.py
    No hubieron cambios mayores en el archivo principal

--- prueba.py
+++ prueba.py
@@ -3,14 +3,44 @@ from dealer import dealer
 from card import card
 from game import game
 
-
-
 def game1():
     while True:
         contador = 0
-        print(f"Starts {self.player_list[contador]}. He/she has {self.player_list[i].hand}"")
-        input("What do you want to play? ")
-        challenge()
+        print(f"Starts {self.player_list[contador]}. He/she has {self.player_list[i].hand}")
+        answer = input("What action do you want to do? ")
+        
+        if answer == "hit":
+            hit()
+        elif answer == "income":
+            income()
+        elif answer == "foreign aid":
+            challenge()
+            # buscar la manera de que si alguien lo desafia, no entre a esta funcion
+            counterattack()
+            foreign_aid()
+
+        elif answer == "duke tax":
+            challenge()
+            duke_tax()
+
+        elif answer == "assassin murder":
+            challenge()
+            counterattack()
+            assassin_murder()
+
+        elif answer == "captain extortion":
+            challenge()
+            counterattack()
+            captain_extortion()
+
+        elif answer == "ambassador change":
+            challenge()
+            counterattack()
+            ambassador_change()
+
+
+
+
 
         contador += 1
         question = int(input("0. Quit "))
@@ -28,5 +58,98 @@ def challenge():
         elif answer == "No":
             continue
     
-xx()
+def counterattack():
+    if answer == "assassin murder":
+        print("Show that you have the card to stop the murderer")
+        contessa_stop_murder()
+
+    elif answer == "captain extortion":
+        print("Show that you have the card to stop the extortion")
+        ambassador_or_captain_stop_extortion()
+
+    elif answer == "duke stop foreign aid":
+        print("Show that you have the card to stop")
+        duke_stop_foreign_aid()
+
+
+
+
+
+def hit():
+    print("This action costs 7 coins.")
+    self.player_list.money - 7
+    input("Who do you want to hit? ")
+    #hay que poder seleccionar al jugador elegido para que se aplique en la funcion turn card
+
+    turn_card()
+
+
+def turn_card():
+    print(f"{self.player_list[i]}, what card do you want to turn?")
+    print(f"options: ")
+    pass
+
+def income():
+    return self.player_list[i].money + 1
+
+def foreign_aid():
+    return self.player_list[i].money + 2
+
+def duke_tax():
+    return self.player[i].money + 3
+
+def assassin_murder():
+    pass
+
+def captain_extortion():
+    pass
+
+def ambassador_change():
+    pass
+
+def duke_stop_foreign_aid()):
+    pass
+
+def contessa_stop_murder():
+    pass
+
+def ambassador_or_captain_stop_extortion():
+    pass
+
+
+
+game1()
+
+if answer == "hit":
+    hit()
+elif answer == "income":
+    income()
+elif answer == "foreign aid":
+    challenge()
+    # buscar la manera de que si alguien lo desafia, no entre a esta funcion
+    counterattack()
+    foreign_aid()
+
+elif answer == "duke tax":
+    challenge()
+    duke_tax()
+
+elif answer == "assassin murder":
+    challenge()
+    counterattack()
+    assassin_murder()
+
+elif answer == "captain extortion":
+    challenge()
+    counterattack()
+    captain_extortion()
+
+elif answer == "ambassador change":
+    challenge()
+    counterattack()
+    ambassador_change()
+
+
+
+
 
