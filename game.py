09b525024f98d77dc3af20305aa7181016f88b5b
feliperiosvaldes <frios1@miuandes.cl>
2021-04-23 15:46:44 -0300

    programa funcionando
    creemos que esta funcionando bien ¡, faltan pequeños detalles

--- game.py
+++ game.py
@@ -160,13 +160,15 @@ class game:
                 return False
 
     def challenge(self,player,value):
+        answer = ""
+        answer2 = ""
         for i in self.player_list:
             if i != player:
                 game_challenge = self.challenge_part1(i)
                 if game_challenge == True:
                     v = self.yes_challenge(player,value)
                     if v == True:
-                        print('You won the challenge')
+                        print(f'\n{player}, you won the challenge')
                         answer = True
                         self.dealer.deck.append(card(value))
                         j = 0
@@ -175,23 +177,25 @@ class game:
                                 player.hand.pop(j)
                             j+=1
                         self.dealer.deal_card(player)
-                        print('You lose, turn up a card!')
+                        print(f'\n{i}, you lost the challenge, turn up a card!')
                         i.look_at_the_hand()
                         answer = int(input('Choose a card: '))
                         self.dealer.dead_deck.append(i.hand[answer])
                         i.hand.pop(answer)
                         break
                     else:
-                        print('You lose, turn up a card:')
+                        print(f'\n {player} you lost the challenge, turn up a card:')
                         answer = int(input('Choose a card: '))
                         self.dealer.dead_deck.append(player.hand[answer])
                         player.hand.pop(answer)
                         answer = False
                         break
                 elif game_challenge == False:
-                    answer = True
+                    answer2 = True
         if answer == True:
             return True
+        elif answer2 == True:
+            return 0
         else:
             return False
     
@@ -209,9 +213,80 @@ class game:
                 if game_counterattack == True:
                     a = self.challenge(i, value)
                     if a == True:
+                        return False
+                    elif a == 0:
                         return True
                     else:
+                        return True
+                else:
+                    continue
+
+    def yes_challenge_with_2_values(self, player, value, value2):
+            player.look_at_the_hand()
+            if len(player.hand) == 2:
+                if player.hand[0].type == value or player.hand[1].type == value or player.hand[0].type == value2 or player.hand[1].type == value2:
+                    return True
+                else:
+                    return False
+            elif len(player.hand) == 1:
+                if player.hand[0].type == value or player.hand[0].type == value2:
+                    return True
+                else:
+                    return False
+
+    def challenge_with_2_values(self, player, value, value2):
+        answer = ""
+        answer2 = ""
+        for i in self.player_list:
+            if i != player:
+                game_challenge = self.challenge_part1(i)
+                if game_challenge == True:
+                    v = self.yes_challenge_with_2_values(player,value, value2)
+                    if v == True:
+                        print(f'\n{player.name}, you won the challenge')
+                        answer = True
+                        #revisar aca
+                        self.dealer.deck.append(card(value))
+                        j = 0
+                        for p in player.hand:
+                            if p.type == value or p.type == value2:
+                                player.hand.pop(j)
+                            j+=1
+                        self.dealer.deal_card(player)
+                        print(f'\n{i.name}, you lost the challenge, turn up a card!')
+                        i.look_at_the_hand()
+                        answer = int(input('Choose a card: '))
+                        self.dealer.dead_deck.append(i.hand[answer])
+                        i.hand.pop(answer)
+                        break
+                    else:
+                        print(f'\n {player.name} you lost the challenge, turn up a card:')
+                        answer = int(input('Choose a card: '))
+                        self.dealer.dead_deck.append(player.hand[answer])
+                        player.hand.pop(answer)
+                        answer = False
+                        break
+                elif game_challenge == False:
+                    answer2 = True
+        if answer == True:
+            return True
+        if answer2 == True:
+            return 0
+        else:
+            return False
+
+    def counterattack_with_2_values(self, player, value, value2):
+          for i in self.player_list:
+            if i != player:
+                game_counterattack = self.counterattack_part1(i)
+                if game_counterattack == True:
+                    a = self.challenge_with_2_values(i, value, value2)
+                    if a == True:
                         return False
+                    elif a == 0:
+                        return True
+                    else:
+                        return True
                 else:
                     continue
 
@@ -246,32 +321,32 @@ class game:
 
                 if select2 == 0:
                     y = self.challenge(player,'Duke')
-                    if y == True:
+                    if y == True or y == 0:
+                        print("You earned 3 coins! ")
                         self.duke_tax(player)
                  
                 elif select2 == 1: 
                     y = self.challenge(player,'Assassin')
-                    if y == True:
+                    if y == True or y == 0:
                         x = self.counterattack(player, 'Contessa')
-                        if x == False:
+                        if x == False or y == 0:
                             print("You avoided the murder succesfully")     
                         else:
                             self.assassin(player)
                     
                 elif select2 == 2:
                     y = self.challenge(player,'Captain')
-                    if y == True:
-                        x = self.counterattack(player, 'Ambassador')
+                    if y == True or y == 0:
+                        x = self.counterattack_with_2_values(player, 'Ambassador', 'Captian')
                         #falta capitan
-                        if x == False:
+                        if x == False or y == 0:
                             print("You avoided the extortion successfully")
                         else:
-                            self.captain_extortion(player)
-                            
+                            self.captain_extortion(player)        
                         
                 elif select2 == 3:
                     y = self.challenge(player,'Ambassador')
-                    if y == True:
+                    if y == True or y == 0:
                         self.ambassador_change(player)
             break    
 
--- prueba.py
+++ prueba.py
@@ -145,6 +145,54 @@ elif answer == "ambassador change":
     ambassador_change()
 
 
+def yes_challenge_with_2_values(self, player, value, value2):
+        player.look_at_the_hand()
+        if len(player.hand) == 2:
+            if player.hand[0].type == value or player.hand[1].type == value or player.hand[0].type == value2 or player.hand[1].type == value2:
 
+                return True
+            else:
+                return False
+        elif len(player.hand) == 1:
+            if player.hand[0].type == value or player.hand[0].type == value2:
+                return True
+            else:
+                return False
+
+     def challenge_with_2_values(self, player, value, value2):
+        for i in self.player_list:
+            if i != player:
+                game_challenge = self.challenge_part1(i)
+                if game_challenge == True:
+                    v = self.yes_challenge_with_2_values(player,value, value2)
+                    if v == True:
+                        print(f'\n{player}, you won the challenge')
+                        answer = True
+                        self.dealer.deck.append(card(value))
+                        j = 0
+                        for p in player.hand:
+                            if p.type == value or p.type == value2:
+                                player.hand.pop(j)
+                            j+=1
+                        self.dealer.deal_card(player)
+                        print(f'\n{i}, you lost the challenge, turn up a card!')
+                        i.look_at_the_hand()
+                        answer = int(input('Choose a card: '))
+                        self.dealer.dead_deck.append(i.hand[answer])
+                        i.hand.pop(answer)
+                        break
+                    else:
+                        print(f'\n {player} you lost the challenge, turn up a card:')
+                        answer = int(input('Choose a card: '))
+                        self.dealer.dead_deck.append(player.hand[answer])
+                        player.hand.pop(answer)
+                        answer = False
+                        break
+                elif game_challenge == False:
+                    answer = True
+        if answer == True:
+            return True
+        else:
+            return False
 
 
